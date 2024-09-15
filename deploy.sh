2c0672ebcd4e70e7e9a408faf23044498bd58655
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-15 11:35:45 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ set -e
 mkdir deploy &&
   mkdir deploy/server &&
   cp -r client/ deploy/client &&
+  rm deploy/client/styles.scss deploy/client/styles.css.map &&
   cd server &&
   GOOS=linux GOARCH=amd64 go build -o ../deploy/server/portfolio.exe server.go &&
   cd .. &&
   rsync -avz deploy/* jfdev:/home/fraterhqc/portfolio &&
   rm -rf deploy
-
