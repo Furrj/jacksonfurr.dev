a9df340109001441c0dcc680a72565f3f7cc3bc7
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-19 10:54:44 -0400

    switch project button working

---
+++ client/main.js
@@ -0,0 +1,12 @@
+const spelldle = document.getElementById("spelldle")
+const taskr = document.getElementById("taskr")
+
+const projects = [spelldle, taskr]
+let count = 0;
+
+function changeProject() {
+	const curr = ++count % projects.length;
+	projects.forEach((p, i) => {
+		p.style.display = curr === i % projects.length ? "block" : "none";
+	})
+}
